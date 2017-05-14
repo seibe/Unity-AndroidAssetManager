d2fb3c14e42a3c2d9349094e45f8199fc29b1a66
Seibe TAKAHASHI <t@seibe.jp>
2017-05-14 21:52:02 +0900

    MonoBehaviourTest を使い Start イベント時にテストするように

--- Assets/Scripts/AndroidAssetManagerTest.cs
+++ Assets/Scripts/AndroidAssetManagerTest.cs
@@ -1,7 +1,9 @@
 ﻿
+#if DEBUG
 using UnityEngine;
 using UnityEngine.TestTools;
 using NUnit.Framework;
+using System;
 
 /// <summary>
 /// Test code for <see cref="Android.AssetManager"/>
@@ -11,20 +13,41 @@ public class AndroidAssetManagerTest
 	[UnityTest]
 	public System.Collections.IEnumerator ReadText()
     {
-		yield return null;
+		yield return new MonoBehaviourTest<Behaviour>();
+	}
+
+    private class Behaviour : MonoBehaviour, IMonoBehaviourTest
+    {
+        private bool _isFinish = false;
+
+        public bool IsTestFinished { get { return _isFinish; } }
+
+        private void Awake()
+        {
+            Debug.Log("Awake");
+        }
+
+        private void Start()
+        {
+            Debug.Log("Start");
 
 #if UNITY_ANDROID
-        using (var stream = Android.AssetManager.Open("Hello.txt"))
-        {
-            var byteLength = stream.ByteLength;
-            Assert.NotZero(byteLength);
+            using (var stream = Android.AssetManager.Open("Hello.txt"))
+            {
+                Assert.NotNull(stream);
 
-            var bytes = stream.ReadAllBytes();
-            Assert.IsNotNull(bytes);
-            Assert.AreEqual(byteLength, bytes.Length);
+                var byteLength = stream.ByteLength;
+                Assert.NotZero(byteLength);
 
-            Debug.LogFormat("Hello.txt = {0} bytes", byteLength);
-        }
+                var bytes = stream.ReadAllBytes();
+                Assert.IsNotNull(bytes);
+                Assert.AreEqual(byteLength, bytes.Length);
+
+                Debug.LogFormat("Hello.txt = {0} bytes", byteLength);
+            }
 #endif
-	}
+            _isFinish = true;
+        }
+    }
 }
+#endif //DEBUG
